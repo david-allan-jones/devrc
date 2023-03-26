4e72d210647753033c6871d5ddff4041fa7e2df5
david-allan-jones <davej1094@yahoo.com>
2023-03-26 22:34:12 +0900

    Create setup bash script to initialize dev environment.

---
+++ README.md
@@ -0,0 +1,11 @@
+# About 
+
+This repo contains personal configuration and rc files for my WSL2
+development environment. To set up the development environment using
+this repository, simply navigate to this repo's root directory and run the below command. 
+
+```bash
+bash wsl_dev_setup.sh
+```
+
+This is a living document based on my personal development needs.
---
+++ wsl_dev_setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+broadcast() {
+	echo -e "\n$1\n"
+}
+
+broadcast "Beginning setup..."
+
+#Pull the latest rc files
+broadcast "Copying rc files..."
+cp .bash_profile ~/.bash_profile
+cp .bashrc       ~/.bashrc
+cp .vimrc        ~/.vimrc
+broadcast "Finished copying rc files."
+
+#Update system
+broadcast "Updating system..."
+sudo apt-get update
+broadcast "Finished updating system."
+
+#Docker
+broadcast "Installing Docker..."
+sudo apt-get install docker.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+broadcast "Finished Docker install."
+
+#Redis
+broadcast "Installing Redis..."
+sudo apt-get install redis -y
+broadcast "Finished Redis install."
+
+#Node
+broadcast "Installing Node..."
+sudo apt-get install nodejs -y
+broadcast "Finished Node install."
+
+#Go
+broadcast "Installing Go..."
+sudo apt-get install golang-go -y
+broadcast "Finished Go install."
+
+broadcast "Dev environment setup complete!"
